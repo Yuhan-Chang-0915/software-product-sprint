6213b8d4cd592e3b502719fa1ea6214a036465a4
yuhan <hannahchang2000@gmail.com>
2022-06-17 19:11:23 +0000

    use dataStore to store data as an entity

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,10 +1,18 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
@@ -15,13 +23,17 @@ public class FormHandlerServlet extends HttpServlet {
     // Get the value entered in the form.
     String name = request.getParameter("name");
     String email = request.getParameter("email");
-    String textValue = request.getParameter("description");
-
-    // Print the value so you can see it in the server logs.
-    System.out.println(name+ " submitted: " + textValue);
-
-    // Write the value to the response so the user can see it.
-    response.getWriter().println(name+" submitted: " + textValue);
-    //response.sendRedirect("/");
+    String message = request.getParameter("description");
+ 
+    // dataStore is the instance of the DataStore class.
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Form");
+    FullEntity formEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("name", name)
+            .set("email", email)
+            .set("message", message)
+            .build();
+    datastore.put(formEntity);
   }
 }
